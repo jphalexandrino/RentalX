2503002eae5f039622ab168a0a5d20be5a9c3c8a
João Pedro Alexandrino <sonderinterprise@gmail.com>
2023-11-07 08:30:44 -0300

    Adicona a configuração do `Debug`

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express, { request, response } from "express";
 
 const app = express();
 
+app.use(express.json);
+
 app.get("/", (request, response) => {
     return response.json({ message: "Hello World" });
+});
+
+app.post("/course", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
 });
 
 app.listen(3333, () => console.log("The Server is running!"));
