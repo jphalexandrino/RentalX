28488fdf6598ce81e3fe869480b43e0403b075de
João Pedro Alexandrino <sonderinterprise@gmail.com>
2023-11-08 18:14:14 -0300

    Adicona as `categorias`

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { request, response } from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("The Server is running!"));
